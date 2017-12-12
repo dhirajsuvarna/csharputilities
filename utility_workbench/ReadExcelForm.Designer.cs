a941f49f703f9b26023f2c2089550ca663bdb5b5
Dhiraj Suvarna <dhiraj.suvarna@gmail.com>
2017-12-12 10:32:17 +0530

    1. Added Link Column with complete functionality 2. Added Search functionality

--- utility_workbench/ReadExcelForm.Designer.cs
+++ utility_workbench/ReadExcelForm.Designer.cs
@@ -33,13 +33,17 @@
             this.browseButton = new System.Windows.Forms.Button();
             this.readExcel = new System.Windows.Forms.Button();
             this.excelDataGridView = new System.Windows.Forms.DataGridView();
+            this.excuteButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.excelDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 28);
+            this.label1.Location = new System.Drawing.Point(48, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(99, 15);
             this.label1.TabIndex = 0;
@@ -47,14 +51,14 @@
             // 
             // excelPathTextBox
             // 
-            this.excelPathTextBox.Location = new System.Drawing.Point(150, 25);
+            this.excelPathTextBox.Location = new System.Drawing.Point(165, 25);
             this.excelPathTextBox.Name = "excelPathTextBox";
-            this.excelPathTextBox.Size = new System.Drawing.Size(292, 21);
+            this.excelPathTextBox.Size = new System.Drawing.Size(634, 21);
             this.excelPathTextBox.TabIndex = 1;
             // 
             // browseButton
             // 
-            this.browseButton.Location = new System.Drawing.Point(471, 25);
+            this.browseButton.Location = new System.Drawing.Point(821, 24);
             this.browseButton.Name = "browseButton";
             this.browseButton.Size = new System.Drawing.Size(37, 23);
             this.browseButton.TabIndex = 2;
@@ -64,7 +68,7 @@
             // 
             // readExcel
             // 
-            this.readExcel.Location = new System.Drawing.Point(227, 89);
+            this.readExcel.Location = new System.Drawing.Point(416, 62);
             this.readExcel.Name = "readExcel";
             this.readExcel.Size = new System.Drawing.Size(75, 23);
             this.readExcel.TabIndex = 3;
@@ -74,27 +78,69 @@
             // 
             // excelDataGridView
             // 
-            this.excelDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.excelDataGridView.AllowUserToAddRows = false;
             this.excelDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.excelDataGridView.Location = new System.Drawing.Point(54, 162);
+            this.excelDataGridView.Location = new System.Drawing.Point(48, 145);
+            this.excelDataGridView.MultiSelect = false;
             this.excelDataGridView.Name = "excelDataGridView";
-            this.excelDataGridView.Size = new System.Drawing.Size(444, 211);
+            this.excelDataGridView.Size = new System.Drawing.Size(956, 286);
             this.excelDataGridView.TabIndex = 4;
+            this.excelDataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.excelDataGridView_CellContentClick);
+            this.excelDataGridView.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.excelDataGridView_CellFormatting);
             // 
-            // ExcelUtilityForm
+            // excuteButton
+            // 
+            this.excuteButton.Location = new System.Drawing.Point(458, 443);
+            this.excuteButton.Name = "excuteButton";
+            this.excuteButton.Size = new System.Drawing.Size(75, 23);
+            this.excuteButton.TabIndex = 5;
+            this.excuteButton.Text = "Excecute";
+            this.excuteButton.UseVisualStyleBackColor = true;
+            this.excuteButton.Click += new System.EventHandler(this.excuteButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(48, 478);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(956, 108);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Search Part Name";
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(165, 104);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(339, 21);
+            this.searchTextBox.TabIndex = 8;
+            this.searchTextBox.TextChanged += new System.EventHandler(this.searchTextBox_TextChanged);
+            // 
+            // ReadExcelForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(541, 409);
+            this.ClientSize = new System.Drawing.Size(1051, 598);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.excuteButton);
             this.Controls.Add(this.excelDataGridView);
             this.Controls.Add(this.readExcel);
             this.Controls.Add(this.browseButton);
             this.Controls.Add(this.excelPathTextBox);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Name = "ExcelUtilityForm";
+            this.Name = "ReadExcelForm";
             this.Text = "ExcelUtilityForm";
             ((System.ComponentModel.ISupportInitialize)(this.excelDataGridView)).EndInit();
             this.ResumeLayout(false);
@@ -109,5 +155,9 @@
         private System.Windows.Forms.Button browseButton;
         private System.Windows.Forms.Button readExcel;
         private System.Windows.Forms.DataGridView excelDataGridView;
+        private System.Windows.Forms.Button excuteButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox searchTextBox;
     }
 }
--- utility_workbench/ReadExcelForm.cs
+++ utility_workbench/ReadExcelForm.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.OleDb;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,14 +34,122 @@ namespace utility_workbench
 
         private void readExcel_Click(object sender, EventArgs e)
         {
-            OleDbConnection connection = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + excelPathTextBox.Text +"; Extended Properties='Excel 8.0;HDR=Yes;IMEX=1'");
-            connection.Open();
+            try
+            {
+                OleDbConnection connection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + excelPathTextBox.Text + ";Extended Properties='Excel 12.0 Xml;HDR=Yes'");
+                //OleDbConnection connection = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + excelPathTextBox.Text + ";Extended Properties='Excel 8.0; HDR = Yes; IMEX = 1'");
+                connection.Open();
 
-            OleDbDataAdapter dataAdapter = new OleDbDataAdapter("select * from [sheet1$]", connection);
-            DataTable dataTable = new DataTable();
-            dataAdapter.Fill(dataTable);
+                OleDbDataAdapter dataAdapter = new OleDbDataAdapter("select * from [sheet1$]", connection);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
 
-            excelDataGridView.DataSource = dataTable.DefaultView;
+                excelDataGridView.DataSource = dataTable.DefaultView;
+
+                AddLinkColumn();
+                CreateUnboundButtonColumn();
+                
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+        }
+
+        private void CreateUnboundButtonColumn()
+        {
+            // Initialize the button column.
+            DataGridViewButtonColumn buttonColumn = new DataGridViewButtonColumn();
+            buttonColumn.Name = "Compare";
+            buttonColumn.HeaderText = "Compare";
+            buttonColumn.Text = "Compare";
+
+            // Use the Text property for the button text for all cells rather
+            // than using each cell's value as the text for its own button.
+            buttonColumn.UseColumnTextForButtonValue = true;
+
+            // Add the button column to the control.
+            excelDataGridView.Columns.Insert(excelDataGridView.ColumnCount, buttonColumn);
+        }
+        private void AddLinkColumn()
+        {
+            DataGridViewLinkColumn linksColumn = new DataGridViewLinkColumn();
+
+            linksColumn.UseColumnTextForLinkValue = false;
+            linksColumn.Name = "Links";
+            linksColumn.HeaderText = "Links";
+            //links.DataPropertyName = ColumnName.ReportsTo.ToString();
+            linksColumn.ActiveLinkColor = Color.White;
+            linksColumn.LinkBehavior = LinkBehavior.SystemDefault;
+            linksColumn.LinkColor = Color.Blue;
+            linksColumn.TrackVisitedState = true;
+            linksColumn.VisitedLinkColor = Color.YellowGreen;
+            //linksColumn.Text = "link";
+
+            excelDataGridView.Columns.Insert(excelDataGridView.ColumnCount, linksColumn);
+        }
+
+        private void excuteButton_Click(object sender, EventArgs e)
+        {
+            DataGridViewRowCollection rows = excelDataGridView.Rows;
+
+            foreach (DataGridViewRow row in rows)
+            {
+                excelDataGridView.Rows[row.Index].Selected = true;
+
+                string partName = row.Cells["Part Name"].Value.ToString();
+                string process = row.Cells["Process"].Value.ToString();
+                string quantity = row.Cells["Quantity"].Value.ToString();
+                string plant = row.Cells["Plant"].Value.ToString();
+                string materialFamily = row.Cells["Material Family"].Value.ToString();
+                string materialGrade = row.Cells["Material Grade"].Value.ToString();
+
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                while (stopWatch.Elapsed < TimeSpan.FromSeconds(5))
+                {
+                }
+                stopWatch.Stop();
+
+                string fileName = partName + ".txt";
+                string[] lines = { partName, process, quantity, plant, materialFamily, materialGrade };
+                File.WriteAllLines(@"C:\Users\dhiraj\Documents\Visual Studio 2015\Projects\utility_workbench\dump\" + fileName, lines);
+
+                string [] files = Directory.GetFiles(@"C:\Users\dhiraj\Documents\Visual Studio 2015\Projects\utility_workbench\dump", fileName);
+                if (files.Length > 0)
+                {
+                    row.Cells["Links"].Value = files[0];
+                    excelDataGridView.Update();
+                    excelDataGridView.Refresh();
+                }
+            }
+        }
+
+        private void excelDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.ColumnIndex == excelDataGridView.Columns["Links"].Index)
+            {
+                string cellValue = excelDataGridView.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();
+                //MessageBox.Show(cellValue);
+                Process.Start(cellValue);
+            }
+        }
+
+        private void excelDataGridView_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+
+            if (excelDataGridView.Columns[e.ColumnIndex].Name.Equals("Links"))
+            {
+                if(e.Value != null)
+                    e.Value = Path.GetFileName(e.Value.ToString()); //change the display name for Hyperlink
+            }
+        }
+
+        private void searchTextBox_TextChanged(object sender, EventArgs e)
+        {
+            DataView dataView = (excelDataGridView.DataSource as DataView);
+            string newStr = string.Format("[Part Name] LIKE '%{0}%'", searchTextBox.Text);
+            dataView.RowFilter = newStr;
         }
     }
 }
